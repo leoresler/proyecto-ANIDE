4a02e0d5d715f76ab2709851684962f102043147
Leandro Resler <leandroresler4@gmail.com>
2025-08-30 12:41:08 -0300

    primer commit: proyecto final Laravel + React

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function () {
+
+    return (
+        <>
+            
+            <h1>home de mi web</h1>
+            
+        </>
+    );
+}
