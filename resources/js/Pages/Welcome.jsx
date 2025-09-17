bb785f6558e1f50c74fab107cdd0180cb2d47865
Leandro Resler <leandroresler4@gmail.com>
2025-09-17 12:36:25 -0300

    cambios login

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,12 +1,16 @@
-import { Head, Link } from '@inertiajs/react';
+import GuestLayout from "@/Layouts/GuestLayout";
+import { Head, Link } from "@inertiajs/react";
 
 export default function () {
-
     return (
-        <>
-            
-            <h1>home de mi web</h1>
-            
-        </>
+        <GuestLayout>
+            <div>
+                <h1>Conecta tu presente con tu futuro</h1>
+
+                <p>Revolucioná la forma de elegir y construir tu camino</p>
+                <br />
+                <p>Encontrá tu vocación en una sola aplicación</p>
+            </div>
+        </GuestLayout>
     );
 }
